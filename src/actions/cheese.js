605e7b7d0563ee33526656053df886b8d30712ed
Janet Leon <leon.irenejanet@gmail.com>
2018-10-12 09:46:35 -0700

    added actions and reducers

--- src/actions/cheese.js
+++ src/actions/cheese.js
@@ -7,9 +7,25 @@ fetch('http://localhost:8080/api/cheeses')
 .catch(error=>console.log(error))
 }
 
-export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
-export const fetchCheeseSuccess = ()=>{
+export const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST'
+export const fetchCheeseRequest = ()=>{
     return {
-        type: FETCH_USER_SUCCESS
+        type: FETCH_USER_REQUEST
+    }
+}
+
+export const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
+export const fetchCheeseSuccess = (cheeses)=>{
+    return {
+        type: FETCH_USER_SUCCESS,
+        cheeses
+    }
+}
+
+export const FETCH_USER_ERROR = 'FETCH_USER_ERROR'
+export const fetchCheeseError = (error)=>{
+    return {
+        type: FETCH_USER_ERROR,
+        error
     }
 }
--- src/reducers/cheese.js
+++ src/reducers/cheese.js
@@ -1,4 +1,5 @@
-import {FETCH_USER_SUCCESS} from '../actions/cheese'
+import {FETCH_USER_SUCCESS, FETCH_USER_REQUEST, FETCH_USER_ERROR} from '../actions/cheese'
+import { isNull } from 'util';
 
 const initialState={
         cheeses: [],
@@ -8,8 +9,12 @@ const initialState={
 };
 
 export const cheeseReducer = (state=initialState, action)=>{
-    if (action.type ===FETCH_USER_SUCCESS ){
-
+    if (action.type ===FETCH_USER_REQUEST ){
+        return Object.assign({}, state, {loading:true})
+    } else if (action.type ===FETCH_USER_SUCCESS ){
+        return Object.assign({}, state, {loading:false, error:null, cheeses: action.cheeses})
+    }else if (action.type ===FETCH_USER_ERROR ){
+        return Object.assign({}, state, {loading:false, error:action.error})
     }
     return state;
 }
